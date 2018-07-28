fbae63180ff8052268de30981481a9af60ed2baa
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-07-28 17:23:08 +0200

    Add pre-rendering

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,31 @@
 const nodeSassMagicImporter = require(`node-sass-magic-importer`);
+const path = require(`path`);
+const PrerenderSpaPlugin = require(`prerender-spa-plugin`);
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, `dist`),
+    routes: [`/`],
+    postProcess(renderedRoute) {
+      // eslint-disable-next-line no-param-reassign
+      renderedRoute.html = renderedRoute.html
+        .replace(/<script (.*?)>/g, `<script $1 defer>`)
+        .replace(`id="app"`, `id="app" data-server-rendered="true"`);
+
+      return renderedRoute;
+    },
+    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+      renderAfterElementExists: `.App`,
+    }),
+  }),
+];
 
 module.exports = {
-  lintOnSave: false,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === `production`) {
+      config.plugins.push(...productionPlugins);
+    }
+  },
   css: {
     loaderOptions: {
       sass: {
@@ -9,4 +33,5 @@ module.exports = {
       },
     },
   },
+  lintOnSave: false,
 };
