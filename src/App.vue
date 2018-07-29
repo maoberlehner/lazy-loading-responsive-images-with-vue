721b0b98f16ab0d9c1d5569add17cb21ec6a96c6
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-07-29 06:01:45 +0200

    Make first image not visible initially

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,20 @@
         dolore magna aliquyam erat, sed diam voluptua. Ergren, no sea
         takimata sanctus est Lorem ipsum dolor sit amet.
       </p>
+      <p>
+        Et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. Ergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+      <p>
+        Diam ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.
+      </p>
       <app-image
         :width="500"
         :height="667"
